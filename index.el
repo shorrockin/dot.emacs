d2b5052c879415efd132da51b08fdd91f30bdf7c
Chris Shorrock <cs@stripe.com>
2018-02-23 12:59:06 -0800

    converted bindings to org-mode

--- index.el
+++ index.el
@@ -4,6 +4,6 @@
 (load "~/.emacs.d/defaults.el")
 (load "~/.emacs.d/packages.el")
 (load "~/.emacs.d/functions.el")
-(load "~/.emacs.d/bindings.el")
+(org-babel-load-file "~/.emacs.d/bindings.org")
 (org-babel-load-file "~/.emacs.d/orgmode.org")
 (load "~/.emacs.private.el" 'missing-ok)
