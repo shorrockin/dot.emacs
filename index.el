fa231e8186d79ffeb7d4568dad0edf38957305f5
Chris Shorrock <cs@stripe.com>
2018-02-23 13:07:09 -0800

    updated functions to use org-mode

--- index.el
+++ index.el
@@ -3,7 +3,7 @@
 (org-babel-load-file "~/.emacs.d/init.org")
 (load "~/.emacs.d/defaults.el")
 (load "~/.emacs.d/packages.el")
-(load "~/.emacs.d/functions.el")
+(org-babel-load-file "~/.emacs.d/functions.org")
 (org-babel-load-file "~/.emacs.d/bindings.org")
 (org-babel-load-file "~/.emacs.d/orgmode.org")
 (load "~/.emacs.private.el" 'missing-ok)
