d4ac62bfcab789dbc6325201e4172930ffcb525e
Chris Shorrock <cs@stripe.com>
2018-02-23 13:58:34 -0800

    converted packages to org-mode

--- index.el
+++ index.el
@@ -2,7 +2,7 @@
 
 (org-babel-load-file "~/.emacs.d/init.org")
 (org-babel-load-file "~/.emacs.d/defaults.org")
-(load "~/.emacs.d/packages.el")
+(org-babel-load-file "~/.emacs.d/packages.org")
 (org-babel-load-file "~/.emacs.d/functions.org")
 (org-babel-load-file "~/.emacs.d/bindings.org")
 (org-babel-load-file "~/.emacs.d/orgmode.org")
